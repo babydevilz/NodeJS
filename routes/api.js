5408114fef0dd81d48d37b5b3ee5e393d415085c
babydevilz <101161425+babydevilz@users.noreply.github.com>
2022-04-13 18:14:20 +0700

    api ok

--- routes/api.js
+++ routes/api.js
@@ -1,44 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-
-const authentication = require('../middle/authentication')
 const userController = require('../components/user/controller');
 const productController = require('../components/products/controller');
 const jwt = require('jsonwebtoken');
+const authentication = require('../middle/authentication');
 
+/**
+ * page: register
+ * http://localhost:3000/api/register
+ * method: post
+ */
+router.post('/register', async function (req, res, next) {
+    const { email, password, confirm_password } = req.body;
+    // kiểm tra email, password
+    const result = await userController.register(email, password, confirm_password);
+    if (result) {
+        res.json({ status: true });
+    } else {
+        res.json({ status: false });
+    }
+});
 
-
-/* page: login */
-// http://localhost:3000/login
-// method: post
+/**
+ * page: login
+ * http://localhost:3000/api/ogin
+ * method: post
+ */
 router.post('/login', async function (req, res, next) {
     const { email, password } = req.body;
     const result = await userController.login(email, password);
     if (result) {
-        const token = jwt.sign({ _id: result._id, email: result.email }, 'myKey');
-        res.json({ status: true, result, token});
-    }
-    else {
+        const token = jwt.sign({ _id: result._id, email: result.email }, 'mykey');
+        res.json({ status: true , result, token });
+    } else {
         res.json({ status: false });
     }
 });
 
-
-/* page: login */
-// http://localhost:3000/api/register
-// method: post
-router.post('/register', async function (req, res, next) {
-
-    const { email, password, confirm_password } = req.body;
-    const result = await userController.register(email, password, confirm_password);
-    if (result) {
-        res.json({ status: true });
-    }
-    else {
-        res.json({ status: false });
-    }
-});
 
 /**
  * page: product
@@ -46,25 +45,21 @@ router.post('/register', async function (req, res, next) {
  * method: get
  * detail: get list products
  */
- router.get('/products', [authentication.checkToken], async function (req, res, next) {
-
+ router.get('/products' ,[authentication.checkToken], async function (req, res, next) {
     // lấy danh sách sản phẩm
     const products = await productController.getProducts();
     res.json(products);
   });
 
-
-  /**
- * page: detail
- * http://localhost:3000/api/products/:id/detail
- * method: get
- * detail: get list products
- */
-router.get('/products/:id/detail', [authentication.checkToken], async function (req, res, next) {
+  router.get('/products/:id/detail' ,[authentication.checkToken], async function (req, res, next) {
     const { id } = req.params;
-    // lấy chi tiết sản phẩm
     const products = await productController.getById(id);
     res.json(products);
-});
+  });
+
+
+
+
+
 
 module.exports = router;
